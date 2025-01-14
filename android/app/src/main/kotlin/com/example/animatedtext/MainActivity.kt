ddfebd0cfe93c067b67950a5f639f5816cc0100e
Fazal Hamza Khan <theunknown.pak@gmail.com>
2025-01-14 22:39:11 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/animatedtext/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.animatedtext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Animated TextKit',
+      home: AnimatedText(),
+    );
+  }
+}
+
+class AnimatedText extends StatelessWidget {
+  const AnimatedText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
